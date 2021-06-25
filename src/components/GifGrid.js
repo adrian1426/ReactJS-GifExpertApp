5b92dcc5dc2d16e428f13762bf9b8bc6ba1e63c2
ADRIAN HERNANDEZ HERNANDEZ <adrian.hernandezhe@elektra.com.mx>
2021-06-24 20:40:50 -0500

    animaciones css

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -7,7 +7,7 @@ const GifGrid = (props) => {
 
   return (
     <>
-      <h3>{category}</h3>
+      <h3 className='animate__animated animate__pulse'>{category}</h3>
       {loading && <p>Loading...</p>}
       <div className='card-grid'>
         {
